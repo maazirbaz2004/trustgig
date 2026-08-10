904081e73f66bf3bfb509b0b9e6fe7b2f71824c5
maazirbaz2004 <maazirbaz2004@gmail.com>
2026-08-10 05:01:15 +0500

    fix: connect to mongodb in vercel entrypoint

--- server/api/index.js
+++ server/api/index.js
@@ -1,2 +1,7 @@
 const app = require("../src/app");
+const connectDB = require("../src/config/db");
+
+// Establish database connection when the serverless function boots up
+connectDB();
+
 module.exports = app;
